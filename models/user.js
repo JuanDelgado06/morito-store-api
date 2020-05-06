cdc5f451fc7cd8552e8db8c7d1634b6588e3af2c
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-05-06 11:41:18 -0500

    Añadiendo circleci y comenzando con la autenticación de usuarios

--- models/user.js
+++ models/user.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const mongooseBcrypt = require('mongoose-bcrypt')
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     name: String,
     email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
     address: { type: Schema.Types.ObjectId, ref: 'Address' }
 });
+
+UserSchema.plugin(mongooseBcrypt);
 
 module.exports = mongoose.model("User", UserSchema);
